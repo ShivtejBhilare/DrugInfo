e8b54092157ac1503f07f2a8f5422b72a7a683d4
Shivtej Bhilare <bhilareshivtejofficial@gmail.com>
2025-03-18 11:36:03 +0530

    changed

--- DrugInfo/VR/settings.py
+++ DrugInfo/VR/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY =os.environ['SECRET_KEY']
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['https://druginfo.pythonanywhere.com/','127.0.0.1','.vercel.app']
+ALLOWED_HOSTS = ['https://druginfo.pythonanywhere.com/','127.0.0.1','.vercel.app','*']
 
 
 # Application definition
